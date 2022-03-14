60f82fddb52398df8b34c66894903207b5b31d1c
MaiCodes-exe <maise.b.costa@gmail.com>
2022-03-14 16:12:54 +0000

    finished last 4 exercises

--- exercises/04.1-conditional-rendering/app.jsx
+++ exercises/04.1-conditional-rendering/app.jsx
@@ -4,15 +4,13 @@ import PropTypes from "prop-types";
 
 const Alert = (props) => {
 	//your component here
-	// let color = "alert-danger";
-	// if (props.color == "red") color = "alert-danger";
+	let color = "alert-primary";
+	if (props.color == "red") color = "alert-danger";
+	else if (props.color == "yellow") color = "alert-warning";
 	return (
 		<div>
-			<div className="alert alert-danger" role="alert">
-				OMG! Something really bad has happended!
-			</div>
-			<div className="alert alert-warning" role="alert">
-				Well, it is not that bad after all!
+			<div className={`alert ${color}`} role="alert">
+				{props.text}
 			</div>
 		</div>
 	);
@@ -27,7 +25,7 @@ Alert.propTypes = {
 ReactDOM.render(
 	<div>
 		<Alert text="OMG! Something really bad has happended!" color="red" />
-		<Alert text="Well, it is not that bad after all!" color="orange" />
+		<Alert text="Well, it is not that bad after all!" color="yellow" />
 	</div>,
 	document.querySelector("#myDiv")
 );
